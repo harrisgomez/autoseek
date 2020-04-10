e6caa76b1eb99d1a2f60799471686797eda5b6ab
Harris G <node.harris@gmail.com>
2020-04-10 12:44:02 -0400

    WIP - Implementing db options and fixing routes.

--- server.js
+++ server.js
@@ -2,54 +2,56 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
-const knex = require('knex')({
+const knex = require('knex');
+
+/* 
+TODO Move options to knex config file
+TODO Refine routes
+TODO Implement Async Bcrypt in /register
+TODO Implement db transaction
+*/
+
+// * DB config
+const db = knex({
     client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'your_database_user',
-        password: 'your_database_password',
-        database: 'myapp_test'
+    connection: 'postgres://postgres:test@localhost:5432/facefacts_db',
+    pool: {
+        min: 0,
+        max: 7,
+        afterCreate: (conn, done) => {
+            conn.query('SET timezone="UTC";', err => {
+                if (err) {
+                    console.log(err);
+                }
+                done(err, conn);
+            });
+        }
     }
 });
+// ! Uncomment & run this query to test for successful connection by returning some data from a known table in our db
+// db.select().from('users').then(console.log).catch(console.error);
 
 
+// * Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const db = {
-    users: [
-        // {
-        //     id: '124',
-        //     name: 'Gary',
-        //     email: 'gary@gmail.com',
-        //     password: 'squirtle',
-        //     entries: 0,
-        //     joined: new Date()
-        // },
-        // {
-        //     id: '123',
-        //     name: 'ash',
-        //     email: 'ash@gmail.com',
-        //     password: 'pika',
-        //     album: [],
-        //     joined: new Date()
-        // }
-    ]
-};
 
+// * Routes
 app.get('/', (req, res) => {
     res.status(200).json(db);
 });
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    const user = db.users.find(userObj => userObj.id === id);
+    // const user = db.users.find(userObj => userObj.id === id);
+    db.select('*').from('users').where({ id }).then(user => console.log(user))
 
     if (user) {
         res.status(200).json(user);
     } else {
-        res.status(404).json('No such user found.');
+        res.status(404).json('No user was found.');
     }
 });
 
@@ -61,33 +63,59 @@ app.post('/signin', (req, res) => {
                 .catch(console.error);
         });
     } else {
-        res.status(400).json('There was a problem signing in');
+        res.status(400).json('There was a problem signing in.');
     }
 });
 
 app.post('/register', (req, res) => {
-    const isEmailTaken = db.users.some(({ email }) => req.body.email === email);
     const { name, email, password } = req.body;
-    const newUser = {
-        id: parseInt(db.users.length + 1),
-        name,
-        email,
-        password,
-        album: []
-    };
+    // const isEmailTaken = db.users.some(({ email }) =>
+    //     req.body.email === email
+    // );
+    // console.log('Email taken?...', isEmailTaken);
+    
+    
+    // if (!isEmailTaken) {
+        const newUser = {
+            name,
+            email,
+            joined: new Date()
+        };
+        
+        db('users')
+            .insert(newUser)
+            // .insert({
+            //     email: email,
+            //     name: name,
+            //     joined: new Date()
+            // })
+            .returning('*')
+            .then(data => res.json(data))
+            .catch(err => res.status(400).json('Error while registering.'));
+    // }
 
-    if (!isEmailTaken) {
-        bcrypt.genSalt(10, (err, salt) => {
-            console.log('salt generated', salt);
-            bcrypt.hash(password, salt, (err, hash) => {
-                newUser.password = hash;
-                db.users.push(newUser);
-                return res.status(200).json(newUser);
-            });
-        });
-    } else {
-        res.status(400).json('That email is unavailable.');
-    }
+    // if (!isEmailTaken) {
+    //     bcrypt.genSalt(10, (err, salt) => {
+    //         console.log('salt generated', salt);
+    //         bcrypt.hash(password, salt, (err, hash) => {
+    //             newUser.password = hash;
+    //             db.users.push(newUser);
+    //             return res.status(200).json(newUser);
+    //         });
+    //     });
+    // } else {
+    //     res.status(400).json('That email is unavailable.');
+    // }
+});
+
+app.put('/album', (req, res) => {
+    const {id} = req.body;
+
+    db('users').where('id', '=', id)
+        .increment('album', 1)
+        .returning('album')
+        .then(album => res.json(album))
+        .catch(err => res.status(400).json('Error updating album.'));
 });
 
 const PORT = process.env.PORT || 8000;

6ebbaf46208c6b13d120c087e474e6a3cdeef6fd
Harris G <node.harris@gmail.com>
2020-04-21 21:13:17 -0400

    Updates

---
+++ precache-manifest.4c4165ecdff4286da557943dc9415c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195317d9144f7e219a95e15a23a004f4",
+    "url": "/face-facts/index.html"
+  },
+  {
+    "revision": "ca068dc02f4ddc747e5e",
+    "url": "/face-facts/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "0f0c44d06617685a4940",
+    "url": "/face-facts/static/css/main.34277974.chunk.css"
+  },
+  {
+    "revision": "ca068dc02f4ddc747e5e",
+    "url": "/face-facts/static/js/2.c0cc688f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-facts/static/js/2.c0cc688f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0c44d06617685a4940",
+    "url": "/face-facts/static/js/main.9ce32f71.chunk.js"
+  },
+  {
+    "revision": "a7d64a4e3c731044acfa",
+    "url": "/face-facts/static/js/runtime-main.162fe0db.js"
+  }
+]);

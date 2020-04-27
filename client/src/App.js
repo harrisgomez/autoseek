5b12e2d1ebb852a3065da0c89744320dd9979f2d
Harris G <node.harris@gmail.com>
2020-04-27 08:03:23 -0400

    Moved App file out to /src directory.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Navigation from '../components/nav/Navigation';
-import Header from '../components/header/Header.js';
-import SignIn from './signIn/SignIn';
-import Register from './register/Register';
-import ImageLinkForm from '../components/imageLinkForm/ImageLinkForm';
-import FaceRecognition from "../components/faceRecognition/FaceRecognition";
+import Navigation from './components/nav/Navigation';
+import Header from './components/header/Header.js';
+import SignIn from './containers/signIn/SignIn';
+import Register from './containers/register/Register';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import FaceRecognition from "./components/faceRecognition/FaceRecognition";
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
-import { particlesConfig } from '../lib';
+import { particlesConfig } from './lib';
 import './App.css';
 import 'tachyons';
 

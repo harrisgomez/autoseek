f1dba27b7a0307d29de3b57ef4546c3b934974e6
Harris G <node.harris@gmail.com>
2020-05-01 14:29:49 -0400

    Resolving "self signed cert" error.

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+// ! Resolves 'Error: self signed certificate' caused by 'SSL: true'. May have to do w/ free heroku acc(?)
+// ! Note this is not secure for production. OK for local dev
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 // * DB CONFIG
 const db = knex({
     client: 'pg',
@@ -23,16 +27,10 @@ const db = knex({
     }
 });
 
-const args = {
-	headers: { "Content-Type": "application/json",  'Authorization': '' },
-	data:{},
-	rejectUnauthorized: false
-};
-
 // * ROUTES
 app.get('/', (_req, res) => res.send('Working'));
 app.post('/signin', signIn.handleSignInRoute(db, bcrypt));
-app.post('/register', args, register.handleRegisterRoute(db, bcrypt));
+app.post('/register', register.handleRegisterRoute(db, bcrypt));
 // app.put('/album', album.handleAlbumRoute(db));
 app.get('/profile/:id', profile.handleProfileRoute(db));
 

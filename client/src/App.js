93c731a8292d522f6d823843b72a43c6bce0a06f
Harris G <node.harris@gmail.com>
2020-05-17 06:56:15 -0400

    Camelcased App file.

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import FaceRecognition from "./components/faceRecognition/faceRecognition";
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import { particlesConfig } from './lib';
-import './App.css';
+import './app.css';
 import 'tachyons';
 
 export const clarifaiApp = new Clarifai.App({

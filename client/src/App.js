345278491af41e840be00bdfc2d8c3d7a8f2d112
Harris G <node.harris@gmail.com>
2020-05-17 17:07:03 -0400

    Fixed syntax error.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import SignIn from './containers/signIn/SignIn';
 import Register from './containers/register/Register';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
-import FaceRecognition from "./components/faceRecognition/FaceRecognition';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import { particlesConfig } from './lib';

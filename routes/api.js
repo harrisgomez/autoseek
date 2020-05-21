5b97d7d58fcf2af8fd5d930f85d5fd88c3e9b892
Harris G <node.harris@gmail.com>
2020-05-21 11:41:42 -0400

    Fixed required sign-in route.

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../database/config');
 const bcrypt = require('bcryptjs');
 const register = require('./register');
-const signIn = require('./signin');
+const signIn = require('./sign-in');
 
 router.get('/', (req, res) => res.json('Homepage'));
 router.post('/signin', signIn.handleSignIn(db, bcrypt));
